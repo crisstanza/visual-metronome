41bef246e956644c0a4a47e734481f1b49a8c5c2
crisstanza <crisstanza@home>
2014-09-18 21:03:37 -0300

    Some looping...

--- js-fail/js.js
+++ js-fail/js.js
@@ -1,24 +1,15 @@
 (function() {
 
 	var MAX_BEATS = 4;
-	var MAX_SUB_BEATS = MAX_BEATS * 2;
+	var MAX_SUB_BEATS = 2;
 
-	var DELAY = 1000;
+	var DELAY = 2000;
 
 	var timer1, timer2, beat1, beat2;
 
 	function previousBeat(beat) {
 		if (beat == 1) {
 			beat = MAX_BEATS;
-		} else {
-			beat--;
-		}
-		return beat;
-	}
-
-	function previousSubBeat(beat) {
-		if (beat == 1) {
-			beat = MAX_SUB_BEATS;
 		} else {
 			beat--;
 		}
@@ -43,18 +34,21 @@
 		return beat;
 	}
 
-	function draw1() {
+	function draw() {
 		var beatPrevious = beat1;
 		if (beatPrevious > 0) {
 			beatPrevious = document.getElementById('beat-'+beatPrevious);
 			Utils.swapClass(beatPrevious, 'On', 'Off');
 			beatPrevious = undefined;
+			var currentSubBeat = document.getElementById('sub-beat-'+beat1+'-'+1);
+			Utils.swapClass(currentSubBeat, 'On', 'Off');
 		}
 		beat1 = nextBeat(beat1);
 		var currentBeat = document.getElementById('beat-'+beat1);
 		Utils.swapClass(currentBeat, 'Off', 'On');
+		draw2();
 		if (timer1 == undefined) {
-			timer1 = setInterval(draw1, DELAY);
+			timer1 = setInterval(draw, DELAY);
 		}
 		//
 		console.log(beat1);
@@ -62,31 +56,23 @@
 	}
 
 	function draw2() {
-		var beatPrevious = beat2;
-		if (beatPrevious > 0) {
-			beatPrevious = document.getElementById('sub-beat-'+beatPrevious);
-			Utils.swapClass(beatPrevious, 'On', 'Off');
-			beatPrevious = undefined;
-		}
 		beat2 = nextSubBeat(beat2);
-		var currentBeat = document.getElementById('sub-beat-'+beat2);
-		Utils.swapClass(currentBeat, 'Off', 'On');
-		if (timer2 == undefined) {
-			timer2 = setInterval(draw2, DELAY / 2);
-		}
 		//
-		console.log(' '+beat2);
+		console.log(' ' + beat2);
 		//
+		var currentSubBeat = document.getElementById('sub-beat-'+beat1+'-'+beat2);
+		Utils.swapClass(currentSubBeat, 'Off', 'On');
+		timer2 = setTimeout(draw2, DELAY / 2);
 	}
 
 	function init() {
-		console.log = function() {};
+		console.log = function() {
+		};
 		timer1 = undefined;
 		timer2 = undefined;
 		beat1 = 0;
 		beat2 = 0;
-		draw1();
-		draw2();
+		draw();
 	}
 
 	addEventListener('load', init, false);
